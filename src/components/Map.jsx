3c12e10478edfbbda61621e58276020830e68aaf
Volodymyr Savchuk <savchuk.v03@gmail.com>
2024-01-17 14:44:10 +0200

    Implement navigation using react router, fake api for storing data (json-server) and fake auth

---
+++ src/components/Map.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+   MapContainer,
+   TileLayer,
+   Marker,
+   Popup,
+   useMap,
+   useMapEvent
+} from "react-leaflet";
+import { useGeolocation } from "../hooks/useGeolocation";
+import { useUrlPosition } from "../hooks/useUrlPosition";
+import { useCities, flagemojiToPNG } from "../contexts/CitiesContext";
+import Button from "./Button";
+import styles from "./Map.module.css";
+
+function Map() {
+   const { cities } = useCities();
+   const [mapPosition, setMapPosition] = useState([50, 30]);
+   const [mapLat, mapLng] = useUrlPosition();
+   const {
+      isLoading: isLoadingPosition,
+      position: geolocationPosition,
+      getPosition
+   } = useGeolocation();
+
+   useEffect(
+      function () {
+         if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+      },
+      [mapLat, mapLng]
+   );
+
+   useEffect(
+      function () {
+         if (geolocationPosition)
+            setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+      },
+      [geolocationPosition]
+   );
+
+   return (
+      <div className={styles.mapContainer}>
+         {!geolocationPosition && (
+            <Button type="position" onClick={getPosition}>
+               {isLoadingPosition ? "Loading..." : "Use your position"}
+            </Button>
+         )}
+         <MapContainer
+            center={mapPosition}
+            zoom={6}
+            scrollWheelZoom={true}
+            className={styles.map}
+         >
+            <TileLayer
+               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+               url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+            />
+            {cities.map((city) => (
+               <Marker
+                  position={[city.position.lat, city.position.lng]}
+                  key={city.id}
+               >
+                  <Popup>
+                     <span>{flagemojiToPNG(city.emoji)}</span>
+                     <span>{city.cityName}</span>
+                  </Popup>
+               </Marker>
+            ))}
+            <ChangeCenter position={mapPosition} />
+            <DetectClick />
+         </MapContainer>
+      </div>
+   );
+}
+
+function ChangeCenter({ position }) {
+   const map = useMap();
+   map.setView(position);
+   return null;
+}
+
+function DetectClick() {
+   const navigate = useNavigate();
+
+   useMapEvent({
+      click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+   });
+
+   return null;
+}
+
+export default Map;
---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,162 @@
+import { useEffect, createContext, useContext, useReducer } from "react";
+
+const BASE_URL = "http://localhost:9000";
+
+const CitiesContext = createContext();
+
+const initialState = {
+   cities: [],
+   isLoading: false,
+   currentCity: {},
+   error: ""
+};
+
+function reducer(state, action) {
+   switch (action.type) {
+      case "loading":
+         return { ...state, isLoading: true };
+      case "cities/loaded":
+         return {
+            ...state,
+            isLoading: false,
+            cities: action.paylaod
+         };
+      case "city/loaded":
+         return {
+            ...state,
+            isLoading: false,
+            currentCity: action.paylaod
+         };
+      case "city/created":
+         return {
+            ...state,
+            isLoading: false,
+            cities: [...state.cities, action.paylaod],
+            currentCity: action.paylaod
+         };
+      case "city/deleted":
+         return {
+            ...state,
+            isLoading: false,
+            cities: state.cities.filter((city) => city.id !== action.paylaod),
+            currentCity: {}
+         };
+      case "rejected":
+         return {
+            ...state,
+            isLoading: false,
+            error: action.paylaod
+         };
+      default:
+         throw new Error("Unknown action type");
+   }
+}
+
+function CitiesProvider({ children }) {
+   const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
+      reducer,
+      initialState
+   );
+
+   useEffect(function () {
+      async function fetchCities() {
+         dispatch({ type: "loading" });
+         try {
+            const res = await fetch(`${BASE_URL}/cities`);
+            const data = await res.json();
+            dispatch({ type: "cities/loaded", paylaod: data });
+         } catch {
+            dispatch({
+               type: "rejected",
+               paylaod: "There was an error loading cities."
+            });
+         }
+      }
+      fetchCities();
+   }, []);
+
+   async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+      dispatch({ type: "loading" });
+      try {
+         const res = await fetch(`${BASE_URL}/cities?id=${id}`);
+         const data = await res.json();
+         dispatch({ type: "city/loaded", paylaod: data[0] });
+      } catch {
+         dispatch({
+            type: "rejected",
+            paylaod: "There was an error loading the city."
+         });
+      }
+   }
+
+   async function createCity(newCity) {
+      dispatch({ type: "loading" });
+      try {
+         const res = await fetch(`${BASE_URL}/cities`, {
+            method: "POST",
+            body: JSON.stringify(newCity),
+            headers: {
+               "Content-Type": "application/json"
+            }
+         });
+         const data = await res.json();
+         dispatch({ type: "city/created", paylaod: data });
+      } catch {
+         dispatch({
+            type: "rejected",
+            paylaod: "There was an error creating the city."
+         });
+      }
+   }
+
+   async function deleteCity(id) {
+      dispatch({ type: "loading" });
+      try {
+         await fetch(`${BASE_URL}/cities/${id}`, {
+            method: "DELETE"
+         });
+         dispatch({ type: "city/deleted", paylaod: id });
+      } catch {
+         dispatch({
+            type: "rejected",
+            paylaod: "There was an error deleting the city."
+         });
+      }
+   }
+
+   return (
+      <CitiesContext.Provider
+         value={{
+            cities,
+            isLoading,
+            error,
+            currentCity,
+            getCity,
+            createCity,
+            deleteCity
+         }}
+      >
+         {children}
+      </CitiesContext.Provider>
+   );
+}
+
+function useCities() {
+   const context = useContext(CitiesContext);
+   if (context === undefined)
+      throw new Error("CitiesContext was used outside the CitiesProvider");
+   return context;
+}
+
+function flagemojiToPNG(flag) {
+   if (!flag) return "";
+   const countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+      .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+      .join("");
+   return (
+      <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+   );
+}
+
+export { CitiesProvider, useCities, flagemojiToPNG };
